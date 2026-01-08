978823c921d4e4cc1f1682c56583d60936f59454
tonnam250 <tonnam250@gmail.com>
2026-01-08 17:30:38 +0700

    Create Login, Register page. Adding font Line Seed Sans TH.

---
+++ nubbill/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nubbill/pages/authentication_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nub-Bill',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        fontFamily: 'LineSeedSansTH',
+      ),
+      home: const AuthenticationPage(),
+    );
+  }
+}
---
+++ nubbill/lib/pages/authentication_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:nubbill/pages/register_page.dart';
+import 'package:nubbill/pages/login_page.dart';
+import '../widgets/rounded_button.dart';
+
+class AuthenticationPage extends StatelessWidget {
+  const AuthenticationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RoundedButton(
+              text: 'สมัครบัญชีใหม่',
+              backgroundColor: Colors.lightBlueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => RegisterPage()),
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            RoundedButton(
+              text: 'ลงชื่อเข้าใช้',
+              backgroundColor: Colors.white,
+              textColor: Colors.lightBlueAccent,
+              outlined: true,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => LoginPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
